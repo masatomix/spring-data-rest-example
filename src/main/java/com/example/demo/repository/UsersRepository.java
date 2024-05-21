a571bc9d101c866cf72c717f1d84fdbffaeb1838
uchihori <uchihori@primebrains.co.jp>
2024-05-21 17:15:37 +0900

    fix

--- src/main/java/com/example/demo/repository/UsersRepository.java
+++ src/main/java/com/example/demo/repository/UsersRepository.java
@@ -1,7 +1,5 @@
 package com.example.demo.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
